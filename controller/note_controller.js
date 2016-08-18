a20c57711151c516cdb85cf80d5c466f03a0c963
prashantmathias <mathiasaudio@gmail.com>
2016-08-18 16:02:35 +0100

    step 6 wip

--- controller/note_controller.js
+++ controller/note_controller.js
@@ -2,20 +2,20 @@
 
   function NoteController(noteList) {
     this._noteList = noteList;
-    // this._note_markup = "";
+    this._note_markup = "";
   }
-  //
-  // NoteController.prototype.setupView = function () {
-  //   var list_view = new NoteListView(this._noteList);
-  //   var note_markup = list_view.getHTML();
-  //   this._note_markup = note_markup;
-  // };
-  //
-  // NoteController.prototype.insertHTML = function (id) {
-  //   var element = document.getElementById(id);
-  //   element.innerHTML = this._note_markup;
-  // };
-  //
-  //
+
+  NoteController.prototype.setupView = function () {
+    var list_view = new NoteListView(this._noteList);
+    var note_markup = list_view.getHTML();
+    this._note_markup = note_markup;
+  };
+
+  NoteController.prototype.insertHTML = function (id) {
+    var element = document.getElementById(id);
+    element.innerHTML = this._note_markup;
+  };
+
+
   exports.NoteController = NoteController;
 })(this);
--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -6,5 +6,41 @@ describe("Controller", function() {
     assert.isTrue(controller._noteList === note_list);
   });
 
-  
+  it("can return a note form within the note list", function() {
+    var note_list = new NoteList();
+    var controller = new NoteController(note_list);
+    note_list.addNote("Favourite drink: seltzer");
+    assert.isTrue(note_list.getNotes()[0].getText() === "Favourite drink: seltzer");
+  });
+
+
+  it("converts list_view into HTML and store it", function () {
+    var note_list = new NoteList();
+    var controller = new NoteController(note_list);
+    note_list.addNote("Favourite drink: seltzer");
+    controller.setupView();
+    assert.isTrue(controller._note_markup === "<ul><li><div>Favourite drink: seltzer</div></li></ul>" );
+  });
+
+  it("can return note's text formatted in HTML", function () {
+
+    var appDiv = document.createElement('div', {id: 'app'} );
+
+    var note_list = new NoteList();
+    var controller = new NoteController(note_list);
+    note_list.addNote("Favourite drink: seltzer");
+    controller.insertHTML("app");
+
+    document.getElementById = function () {
+      return appDiv;
+    }
+    document.getElementById = document.__proto__.getElementById;
+
+    var element = document.getElementById("app");
+    assert.isTrue(element.innerHTML === "<ul><li><div>Favourite drink: seltzer</div></li></ul>" );
+
+
+
+
+  });
 });
